be4da51b86f64f53642b5fe408202e7d5032fc79
gogo1701 <30569429+gogo1701@users.noreply.github.com>
2024-12-02 14:33:46 +0200

    Added classes, enums and main file + sln.

---
+++ GameOfWar/GameOfWar/CardSuit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfWar
+{
+    public enum CardSuit
+    {
+        Spade = '♠',
+        Club = '♣',
+        Heart = '♥',
+        Diamond = '♦'
+    }
+}
