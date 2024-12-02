bf0eb85dae57536cf91fe6e5be7166de939543f3
IvailoKirilov <ivaylo.kirilov.highschool@buditel.bg>
2024-12-02 14:50:39 +0200

    Add GenerateDeck and ProcessWar functions

--- GameOfWar/GameOfWar/GameOfWar.cs
+++ GameOfWar/GameOfWar/GameOfWar.cs
@@ -23,3 +23,62 @@ Console.WriteLine(@"
 ||                                                                            ||
 || Have fun!                                                                  ||
 ================================================================================");
+
+
+
+
+List<Card> GenerateDeck()
+{
+    List<Card> deck = new List<Card>();
+    CardFace[] faces = (CardFace[])Enum.GetValues(typeof(CardFace));
+    CardSuit[] suits = (CardSuit[])Enum.GetValues(typeof(CardSuit));
+
+    for (int suite = 0; suit < suits.Lenght; suite++)
+    {
+        for(int face = 0; face < faces.Lenght; face++)
+        {
+            CardFace currentFace = faces[face];
+            CardSuit currentSuit = suits[suite];
+            deck.Add(new Card
+            {
+                Face = currentFace,
+                Suite = currentSuit
+
+            });
+        }
+    }
+    return deck;
+}
+
+void ProcessWar(Queue<Card> pool)
+{
+    while ((int)firstPlayerCard.Face == (int)secondPlayerCard.Face)
+    {
+        Console.WriteLine("WAR!");
+
+        if (firstPlayerDeck.Count < 4)
+        {
+            AddCardsToWinnerDeck(firstPlayerDeck, secondPlayerDeck);
+            Console.WriteLine($"First player does not have enough cards to contunue playing...");
+            break;
+        }
+        if (secondPlayerDeck.Count < 4)
+        {
+            AddCardsToWinnerDeck(secondPlayerDeck, firstPlayerDeck);
+            Console.WriteLine($"Second player does not have enough cards to contunue playing...");
+            break;
+        }
+
+        AddWarCardsToPool(pool);
+
+        firstPlayerCard = firstPlayerDeck.Dequeue();
+        Console.WriteLine($"First player has drawn: {firstPlayerCard}");
+
+        secondPlayerCard = secondPlayerDeck.Dequeue();
+        Console.WriteLine($"Second player has drawn: {secondPlayerCard}");
+
+        pool.Enqueue(firstPlayerCard);
+        pool.Enqueue(secondPlayerCard);
+
+    }
+}
